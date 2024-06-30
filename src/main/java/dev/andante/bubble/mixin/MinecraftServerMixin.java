80f0cc3abe04d2b602165700372ebb3768c8cdad
Daniel Orr <orrdaniel73@gmail.com>
2024-07-01 00:00:08 +0100

    Improve world save filtering
    (From a Brawls standpoint)

--- src/main/java/dev/andante/bubble/mixin/MinecraftServerMixin.java
+++ src/main/java/dev/andante/bubble/mixin/MinecraftServerMixin.java
@@ -8,43 +8,31 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
     @Shadow @Final public Map<RegistryKey<World>, ServerWorld> worlds;
 
-    @Unique
-    private final Map<RegistryKey<World>, IBubbleWorld> temporaryBubbleWorldsStorage = new HashMap<>();
-
     /**
      * Removes bubble worlds before saving.
      */
-    @SuppressWarnings("resource")
-    @Inject(method = "save", at = @At("HEAD"))
-    private void onSaveHead(boolean suppressLogs, boolean flush, boolean force, CallbackInfoReturnable<Boolean> cir) {
-        temporaryBubbleWorldsStorage.clear();
-
-        new HashMap<>(worlds).forEach((key, world) -> {
-            if (world instanceof IBubbleWorld bubbleWorld) {
-                temporaryBubbleWorldsStorage.put(key, bubbleWorld);
-                worlds.remove(key);
-            }
-        });
-    }
-
-    /**
-     * Add bubble worlds back after saving.
-     */
-    @Inject(method = "save", at = @At("TAIL"))
-    private void onSaveTail(boolean suppressLogs, boolean flush, boolean force, CallbackInfoReturnable<Boolean> cir) {
-        temporaryBubbleWorldsStorage.forEach((key, world) -> worlds.put(key, world.asServerWorld()));
-        temporaryBubbleWorldsStorage.clear();
+    @Redirect(
+            method = "save",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/MinecraftServer;getWorlds()Ljava/lang/Iterable;",
+                    ordinal = 0
+            )
+    )
+    private Iterable<ServerWorld> onSaveWorlds(MinecraftServer server) {
+        return this.worlds
+                .values()
+                .stream()
+                .filter(world -> !(world instanceof IBubbleWorld))
+                .toList();
     }
 }
